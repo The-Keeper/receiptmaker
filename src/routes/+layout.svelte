5a78f53dda1d6cb66783513880c01397ff07f540
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-10-31 18:56:18 +0500

    flowbite

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,33 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
-</script>
 
-{@render children()}
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+
+	import { DarkMode } from 'flowbite-svelte';
+
+	let btnClass = 'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xl p-2';
+  </script>
+  
+
+  <Navbar>
+	<NavBrand href="/">
+	  <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+	</NavBrand>
+	<NavHamburger  />
+	<NavUl >
+	  <NavLi href="/settings">Settings</NavLi>
+	  <NavLi href="/store">Store</NavLi>
+	</NavUl>
+
+	<DarkMode {btnClass} />
+
+  </Navbar>
+
+
+<div class="flex px-4 mx-auto w-full">
+	<main class="lg:ml-72 w-full mx-auto">
+		{@render children()}
+	</main>
+</div>
+
---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">  
+    import { items, settings, order } from "$lib/store";
+    
+    import Settings from "../../components/Settings.svelte";
+
+</script>
+
+<Settings></Settings>
