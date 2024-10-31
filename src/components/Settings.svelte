26f0ef67d8031995141fcbcb3161eb597a9a5bb9
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-10-31 23:06:48 +0500

    formatting

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -2,14 +2,13 @@
 	import { settings, items } from '$lib/store';
 	import { Label, Input, Select } from 'svelte-5-ui-lib';
 
-	const currencies = Intl.supportedValuesOf("currency");
-	const currencyValues = currencies.map(c => {
+	const currencies = Intl.supportedValuesOf('currency');
+	const currencyValues = currencies.map((c) => {
 		return {
 			value: c,
 			name: c
-		}
-	})
-
+		};
+	});
 </script>
 
 <h3>Настройки</h3>
@@ -17,12 +16,21 @@
 <div>
 	<div>
 		<Label for="name" class="mb-2">Название места</Label>
-		<Input type="text" id="name" required bind:value={ $settings.name }/>
-	  </div>
-
-	  <div>
-		<Label for="name" class="mb-2">Валюта</Label>
-	  	<Select bind:value={ $settings.currency } items={currencyValues} placeholder="Выберите валюту" class="!rounded-s-none" />
+		<Input type="text" id="name" required bind:value={$settings.name} />
 	</div>
 
+	<div>
+		<Label for="name" class="mb-2">Адрес</Label>
+		<Input type="text" id="name" required bind:value={$settings.address} />
+	</div>
+
+	<div>
+		<Label for="name" class="mb-2">Валюта</Label>
+		<Select
+			bind:value={$settings.currency}
+			items={currencyValues}
+			placeholder="Выберите валюту"
+			class="!rounded-s-none"
+		/>
+	</div>
 </div>
