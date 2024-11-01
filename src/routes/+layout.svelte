c21a51482a03fe1f55dbf09500b0f34404442099
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-11-02 03:40:21 +0500

    classes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,6 @@
 	import { ReceiptSolid, StoreSolid, UserSettingsSolid } from 'flowbite-svelte-icons';
 	import { Darkmode } from 'svelte-5-ui-lib';
 	let spanClass = 'flex-1 ms-3 whitespace-nowrap';
-
-	let btnClass = 'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xl p-2';
   
 	const sidebarUi = uiHelpers();
 	let isSidebarOpen = $state(false);
@@ -25,14 +23,14 @@
 </header>
 
   <div class="overflow-hidden lg:flex">
-  <Sidebar isOpen={isSidebarOpen} closeSidebar={sidebarUi.close}  activeClass="p-2" nonActiveClass="p-2">
+  <Sidebar isOpen={isSidebarOpen} closeSidebar={sidebarUi.close} activeClass="p-2" nonActiveClass="p-2">
 	  <SidebarGroup>
-		<SidebarItem label="Чеки" href="/receipts" active={ activeUrl === '/receipts' }>
+		<SidebarItem label="Чеки" href="/receipts/">
 			{#snippet iconSlot()}
 				<ReceiptSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
 			{/snippet}
 		  </SidebarItem>
-		<SidebarItem label="Склад" {spanClass}  href="/store" active={activeUrl === '/store'}>
+		<SidebarItem label="Склад" {spanClass}  href="/store/">
 			{#snippet iconSlot()}
 			<StoreSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
 			{/snippet}
@@ -40,7 +38,7 @@
 			<span class="inline-flex justify-center items-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300"> { $items.length } </span>
 			{/snippet}
 		</SidebarItem>
-		<SidebarItem label="Настройки" href="/settings" active={activeUrl === '/settings'}>
+		<SidebarItem label="Настройки" href="/settings/">
 			{#snippet iconSlot()}
 			  <UserSettingsSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
 			  {/snippet}
