2ec9b7b61797555ca41f9eb3c072f34a2a6d7a15
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-11-24 03:28:37 +0500

    styling

--- src/components/SelectionPanel.svelte
+++ src/components/SelectionPanel.svelte
@@ -4,7 +4,7 @@
 
 <div class="w-full items-grid gap-2">
 	{#each $items as item, index}
-	<button onclick={ () => addOrderItem(item) } type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+	<button onclick={ () => addOrderItem(item) } type="button" class="p-inline-2 p-block-3 text-center text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
 		{ item.title }</button>
 	{/each}
 </div>
--- uno.config.ts
+++ uno.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 		],
 	},
 	shortcuts: {
-		"title": 'text-4xl font-extrabold dark:text-white',
+		"title": 'text-3xl font-bold dark:text-white',
 		"input": 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 	},
 	presets: [presetUno()],
