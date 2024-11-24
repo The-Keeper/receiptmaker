51a74708e329ebf477d37839a563fc6272426e55
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-11-24 20:55:50 +0500

    responsive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,20 +7,26 @@
 
 	import { base } from '$app/paths';
 	import { page } from '$app/stores';
+	import { ReceiptOutline, StoreOutline, UserSettingsOutline } from 'flowbite-svelte-icons';
 
-	const activeUrl = $state($page.url.pathname);
+	const activeUrl = $derived($page.url.pathname);
 
  </script>
+
+{#snippet navitem(href: string)}
+	<a class="flex gap-1" {href}>
+		{@render children()}
+	</a>
+{/snippet}
+
   
-<header class="p-3">
-	<div class="flex justify-center gap-4">
-		<a href={`${base}/receipts`}>Чеки</a>
-		<a href={`${base}/store`}>Склад</a>
-		<a href={`${base}/settings`}>Настройки</a>
+	<div class="relative flex justify-center gap-2 p-3 flex-wrap md:p-5 md:w-64 md:fixed md:flex-col">
+		<a class="flex p-2 rounded-2 {activeUrl===`${base}/receipts` ? "bg-gray-400" : ""}" href={`${base}/receipts`}><ReceiptOutline/> Чеки</a>
+		<a class="flex p-2 rounded-2 {activeUrl===`${base}/store` ? "bg-gray-400" : ""}" href={`${base}/store`}><StoreOutline /> Склад</a>
+		<a class="flex p-2 rounded-2 {activeUrl===`${base}/settings` ? "bg-gray-400" : ""}" href={`${base}/settings`}><UserSettingsOutline /> Настройки</a>
 	</div>
-</header>
 
 
-	<main class="h-full w-full overflow-y-auto">
+	<main class="md:ml-64 p-4">
 		{@render children()}
 	</main>
