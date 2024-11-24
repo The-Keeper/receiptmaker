f0c27d64ff6faaf03e4ee84ed389392a4fc129e6
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-11-24 21:13:31 +0500

    nav item

---
+++ src/components/NavItem.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+    const { children, href }: { children: Snippet<[]>; href: string; } = $props();
+
+
+	import { page } from '$app/stores';
+	const activeUrl = $derived($page.url.pathname);
+
+ </script>
+
+
+<a class="flex p-2 rounded-2 {activeUrl===href ? "bg-gray-700" : ""}" {href}>{@render children()}</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,22 +8,17 @@
 	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 	import { ReceiptOutline, StoreOutline, UserSettingsOutline } from 'flowbite-svelte-icons';
+	import NavItem from '../components/NavItem.svelte';
 
 	const activeUrl = $derived($page.url.pathname);
 
  </script>
 
-{#snippet navitem(href: string)}
-	<a class="flex gap-1" {href}>
-		{@render children()}
-	</a>
-{/snippet}
-
   
 	<div class="relative flex justify-center gap-2 p-3 flex-wrap md:p-5 md:w-64 md:fixed md:flex-col">
-		<a class="flex p-2 rounded-2 {activeUrl===`${base}/receipts` ? "bg-gray-700" : ""}" href={`${base}/receipts`}><ReceiptOutline/> Чеки</a>
-		<a class="flex p-2 rounded-2 {activeUrl===`${base}/store` ? "bg-gray-700" : ""}" href={`${base}/store`}><StoreOutline /> Склад</a>
-		<a class="flex p-2 rounded-2 {activeUrl===`${base}/settings` ? "bg-gray-700" : ""}" href={`${base}/settings`}><UserSettingsOutline /> Настройки</a>
+		<NavItem href={`${base}/receipts`}><ReceiptOutline/> Чеки</NavItem>
+		<NavItem href={`${base}/store`}><StoreOutline /> Склад</NavItem>
+		<NavItem href={`${base}/settings`}><UserSettingsOutline /> Настройки</NavItem>
 	</div>
 
 
