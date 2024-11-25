7cd0b6524083b6f7bdfb5c5057298331a47b2124
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-11-25 14:52:21 +0500

    initial i18n

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import "@unocss/reset/tailwind.css";
 	import "virtual:uno.css";
 
+	import * as t from "$t18s/messages";
+
 	const { children } = $props();
 
 	import { base } from '$app/paths';
@@ -13,9 +15,9 @@
 
   
 	<div class="relative flex justify-center gap-2 p-3 flex-wrap md:p-5 md:w-64 md:fixed md:flex-col">
-		<NavItem href={`${base}/receipts`}><ReceiptOutline/> Чеки</NavItem>
-		<NavItem href={`${base}/store`}><StoreOutline /> Склад</NavItem>
-		<NavItem href={`${base}/settings`}><UserSettingsOutline /> Настройки</NavItem>
+		<NavItem href={`${base}/receipts`}><ReceiptOutline/> {t.receipts()}</NavItem>
+		<NavItem href={`${base}/store`}><StoreOutline /> {t.store()}</NavItem>
+		<NavItem href={`${base}/settings`}><UserSettingsOutline /> {t.settings()}</NavItem>
 	</div>
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import unocss from 'unocss/vite';
+import { t18s } from "t18s";
 
 export default defineConfig({
-	plugins: [unocss(), sveltekit()]
+	plugins: [
+		unocss(), 
+		sveltekit(),
+		t18s({
+			locales: ["en", "ru"],
+			fallbackLocale: "ru",
+		}),	  
+	]
 });
